e85bbca01aba4e9bad374745c9fef1445c449793
Jonghwi Lee <zziller03@gmail.com>
2020-05-24 17:26:55 +0900

    Create ExpressionByN.java

---
+++ src/programmers/dp/ExpressionByN.java
@@ -0,0 +1,50 @@
+package programmers.dp;
+
+import java.util.*;
+
+/**
+ * level 3
+ * https://programmers.co.kr/learn/courses/30/lessons/42895
+ */
+public class ExpressionByN {
+	public static void main(String[] args) {
+		int N = 5;
+		int number = 26;
+		System.out.println(solution(N, number));
+	}
+	public static int solution(int N, int number) {
+		int answer = -1;
+		Set<Integer>[] setArr = new Set[9];
+		int t = N;
+		for(int i = 1; i < 9; i++) {
+			setArr[i] = new HashSet<>();
+			setArr[i].add(t);
+			t = t * 10 + N;
+		}
+		for(int i = 1; i < 9; i++) {
+			for(int j = 1; j < i; j++) {
+				for(Integer a : setArr[j]) {
+					for(Integer b : setArr[i - j]) {
+						setArr[i].add(a + b);
+						setArr[i].add(a - b);
+						setArr[i].add(b - a);
+						setArr[i].add(a * b);
+						if(b != 0) {
+							setArr[i].add(a / b);
+						}
+						if(a != 0) {
+							setArr[i].add(b / a);
+						}
+					}
+				}
+			}
+		}
+		for(int i = 1; i < 9; i++) {
+			if(setArr[i].contains(number)) {
+				answer = i;
+				break;
+			}
+		}
+		return answer;
+	}
+}
